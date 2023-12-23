f7df4505462763cb624b23866f7cb2f78acf1013
nati hadad <85611969+NatiHadad007@users.noreply.github.com>
2023-12-23 13:46:59 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,45 @@
+function init() {
+  let updated = localStorage.getItem("updated");
+  console.log(updated);
+  if (updated == undefined) {
+    getPromo();
+  } else if (Date.now() - updated > 86400000) {
+    getPromo();
+  } else {
+    document.getElementById("extractedText").innerHTML =
+      localStorage.getItem("PromoString");
+    console.log("promo updated");
+  }
+}
+
+async function getPromo() {
+  const date = new Date();
+
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  let currentDate = `${day}.${month}.${year}`; //faf91e38869ace231718ca2c7b136f53
+  const response = await fetch(
+    "http://api.scrapestack.com/scrape?access_key={your_scrapestack_key}&url=http://www.saatva.com"
+  )
+    .then((response) => response.text())
+    .then((html) => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const display = doc.querySelector(".u-hidden--md-down").textContent;
+      let PromoString =
+        "<strong>" +
+        `Saatva promotion of (${currentDate}): ` +
+        "</strong>" +
+        display;
+      document.getElementById("extractedText").innerHTML = PromoString;
+      localStorage.setItem("PromoString", PromoString);
+      localStorage.setItem("updated", Date.now());
+    })
+    .catch((error) => {
+      console.log("An error occurred while fetching the website:", error);
+    });
+}
+
+init();
